a401cfbb9c470aad052f0ab21c5cd16a089f24ef
Stephen Crowley <crow@outsider.trading>
2023-12-17 18:25:00 -0600

    remove selenium stuff

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,13 +3,7 @@ module chatgptapi
   requires java.base;
 
   requires transitive java.desktop;
-  requires transitive java.instrument;
-  requires transitive java.logging;
   requires transitive javafx.controls;
-  requires transitive com.google.common;
-  requires transitive org.seleniumhq.selenium.api;
-  requires transitive org.seleniumhq.selenium.firefox_driver;
-  requires transitive dev.failsafe.core;
   requires transitive javafx.graphics;
   
   exports ai.open.chatgpt;
